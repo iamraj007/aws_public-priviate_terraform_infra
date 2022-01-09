87d194c52b2b959eb2f285aeb2d282d3e860766b
iamraj007 <47947075+iamraj007@users.noreply.github.com>
2022-01-09 20:31:18 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,22 +1,29 @@
 resource "aws_instance" "Public-bastion-instance" {
   ami           = "ami-0affd4508a5d2481b"
   instance_type = "t2.nano"
-  subnet_id = "${aws_subnet.Public-subnet.id}"
+  subnet_id     = "${aws_subnet.Public-subnet.id}"
   security_groups = ["${aws_security_group.My-sg-ssh-allow.id}"]
+#  security_group_id  = [aws_security_group.My-sg-ssh-allow.id, aws_security_group.My-sg-http-allow-pub.id]
+
   key_name = "infra-home" 
+  
   tags = {
-    Name = "Public-bastion"
+    Name        = "Public-bastion"
+    Environment = "DEV"
+    Onwer       = "Me"  
+#    Created     = formatdate("DD MMM YYYY", timestamp())    
   }
 }
 
 resource "aws_instance" "Private-instance1" {
   ami           = "ami-0affd4508a5d2481b" 
   instance_type = "t2.nano"
-  subnet_id = "${aws_subnet.Private-subnet.id}"
+  subnet_id     = "${aws_subnet.Private-subnet.id}"
   security_groups = ["${aws_security_group.My-sg-ssh-allow.id}"]
-  key_name = "infra-home"
+  key_name      = "infra-home"
   tags = {
-    Name = "Private-instance01"
+    Name        = "Private-instance01"
+    Onwer       = "Me"  
   }
   depends_on = ["aws_nat_gateway.MyNAT-GW"]
 }
@@ -25,11 +32,12 @@ resource "aws_instance" "Private-instance1" {
 resource "aws_instance" "Private-instance2" {
   ami           = "ami-0affd4508a5d2481b"
   instance_type = "t2.nano"
-  subnet_id = "${aws_subnet.Private-subnet2.id}"
+  subnet_id     = "${aws_subnet.Private-subnet2.id}"
   security_groups = ["${aws_security_group.My-sg-ssh-allow.id}"]
-  key_name = "infra-home"
+  key_name      = "infra-home"
   tags = {
     Name = "Private-instance02"
+    Onwer       = "Me"  
   }
   depends_on = ["aws_nat_gateway.MyNAT-GW"]
 }
