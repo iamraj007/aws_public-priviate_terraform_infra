3884baf9ea8eec6314a6e690ec28a2cec6cd3dc0
iamraj007 <47947075+iamraj007@users.noreply.github.com>
2022-01-09 20:23:53 +0530

    Added web ports to SG

--- security_group.tf
+++ security_group.tf
@@ -20,3 +20,39 @@ resource "aws_security_group" "My-sg-ssh-allow" {
   }
 }
 
+
+resource "aws_security_group" "My-sg-http-allow-pub" {
+  vpc_id      = "${aws_vpc.MyVPC.id}"
+  name        = "http-allow-pub-${var.env}"
+  description = "Allow HTTP Port Ingress from pub!"
+
+  egress {
+    description = "Allow out traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow http ingress"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow http secure port ingress"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name           = "HTTP-Allow-Pub"
+    Internetfacing = "Yes"
+  }
+}
+
+
